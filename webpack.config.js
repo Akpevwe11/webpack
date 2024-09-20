797fed8d0c90d2216225cf59944fc5e07f71f1a3
Akpevwe <essiakpevwepraise@gmail.com>
2024-09-20 17:30:02 +0100

    dev server

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,14 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }, 
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        compress: true,
+        port: 9000
+    },
     module: {
         rules: [
             {
